60cbad1cfe98be16df60c609efa9b9942014185f
SoolkingSami <barbutdicas@gmail.com>
2022-03-28 09:48:42 +0300

    feat(dsa): add backtracking traversal for king and knight

--- 1st year/2nd sem/DSA Problems/backtracking/maze.py
+++ 1st year/2nd sem/DSA Problems/backtracking/maze.py
@@ -5,8 +5,9 @@ dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
 def print_solution(sol):
     for i in sol:
         for j in i:
-            print(str(j) + " ", end="")
-        print("")
+            print(f'{j:<5}', end="")
+        print()
+    print()
 
 
 def is_safe(maze_local, x, y, sol):
@@ -17,17 +18,21 @@ def is_safe(maze_local, x, y, sol):
 
 def solve_maze(maze_local):
     sol = [[0 for _ in range(4)] for _ in range(4)]
+    print(f"The initial maze:")
+    print_solution(maze_local)
 
     if not solve_maze_util(maze_local, 0, 0, sol):
         print("There is no solution")
         return False
 
+    print(f"Final solution from (0, 0) to ({N - 1}, {N - 1}):")
     print_solution(sol)
     return True
 
 
 def solve_maze_util(maze_local, x, y, sol):
-    print(f'We are currently in ({x}, {y})')
+    print(f'We are currently in ({x}, {y}):')
+    print_solution(sol)
 
     if x == N - 1 and y == N - 1:
         sol[x][y] = 1
@@ -45,4 +50,7 @@ def solve_maze_util(maze_local, x, y, sol):
 
 
 maze = [[1, 0, 0, 0], [1, 1, 0, 1], [0, 1, 0, 0], [1, 1, 1, 1]]
-solve_maze(maze)
+# with open("maze.txt", "rt") as f:
+#     N = int(next(f))
+#     start_x, start_y, end_x, end_y =
+# solve_maze(maze)
